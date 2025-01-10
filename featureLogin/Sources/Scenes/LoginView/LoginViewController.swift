c7ca8398850722109f31e6068b51c3c56150468d
jeanramalho.dev <jeanramalho.dev@gmail.com>
2025-01-10 11:07:25 -0300

    [add] - realiza configurações iniciais da tela de login

--- featureLogin/Sources/Scenes/LoginView/LoginViewController.swift
+++ featureLogin/Sources/Scenes/LoginView/LoginViewController.swift
@@ -4,4 +4,37 @@
 //
 //  Created by Jean Ramalho on 10/01/25.
 //
+import Foundation
+import UIKit
 
+class LoginViewController: UIViewController {
+    let contentView: LoginView
+    
+    init(contentView: LoginView) {
+        self.contentView = contentView
+        super.init(nibName: nil, bundle: nil)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        setup()
+    }
+    
+    private func setup(){
+        self.view.addSubview(contentView)
+        setupConstraints()
+    }
+    
+    private func setupConstraints(){
+        NSLayoutConstraint.activate([
+            contentView.topAnchor.constraint(equalTo: view.topAnchor),
+            contentView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+            contentView.trailingAnchor.constraint(equalTo: view.trailingAnchor),
+            contentView.bottomAnchor.constraint(equalTo: view.bottomAnchor)
+        ])
+    }
+}
