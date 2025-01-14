d5c2e5793aab5fd4b61ff211bdf65d2af232df72
jeanramalho.dev <jeanramalho.dev@gmail.com>
2025-01-14 10:18:08 -0300

    [add] - Cria todos os elementos da tela de login

--- featureLogin/Sources/Scenes/LoginView/LoginView.swift
+++ featureLogin/Sources/Scenes/LoginView/LoginView.swift
@@ -8,6 +8,55 @@ import Foundation
 import UIKit
 
 class LoginView: UIView {
+    
+    lazy var viewLogin: UIView = {
+        let view = UIView()
+        view.translatesAutoresizingMaskIntoConstraints = false
+        return view
+    }()
+    
+    lazy var logoImageView: UIImageView = {
+        let image = UIImageView()
+        image.translatesAutoresizingMaskIntoConstraints = false
+        image.image = UIImage(named: "logo")
+        image.contentMode = .scaleAspectFit
+        return image
+    }()
+    
+    lazy var emailLabel: UILabel = {
+        let label = UILabel()
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.font = UIFont(name: "Rubik-Bold", size: 14)
+        label.numberOfLines = 0
+        return label
+    }()
+    
+    lazy var passwordLabel: UILabel = {
+        let label = UILabel()
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.font = UIFont(name: "Rubik-Bold", size: 14)
+        return label
+    }()
+    
+    lazy var emailTextField: UITextField = {
+        let textField = UITextField()
+        textField.translatesAutoresizingMaskIntoConstraints = false
+        textField.borderStyle = .roundedRect
+        textField.layer.cornerRadius = 4
+        textField.placeholder = "Digite seu email"
+        return textField
+    }()
+    
+    lazy var passwordTextField: UITextField = {
+        let textField = UITextField()
+        textField.translatesAutoresizingMaskIntoConstraints = false
+        textField.borderStyle = .roundedRect
+        textField.layer.cornerRadius = 4
+        textField.placeholder = "Digite sua senha"
+        textField.isSecureTextEntry = true
+        return textField
+    }()
+    
     override init(frame: CGRect) {
         super.init(frame: frame)
         setupUI()
